1b44d0757db732bdbe472c2fff0926d1df5c92d0
Cesar Blum Silveira <cesars@microsoft.com>
2017-03-17 14:10:12 -0700

    Remove deprecated property KestrelServerOptions.MaxRequestBufferSize (#1027).

--- test/Microsoft.AspNetCore.Server.KestrelTests/KestrelServerOptionsTests.cs
+++ test/Microsoft.AspNetCore.Server.KestrelTests/KestrelServerOptionsTests.cs
@@ -2,9 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Linq;
 using System.Net;
-using System.Reflection;
 using Microsoft.AspNetCore.Server.Kestrel;
 using Xunit;
 
@@ -12,34 +10,6 @@ namespace Microsoft.AspNetCore.Server.KestrelTests
 {
     public class KestrelServerOptionsTests
     {
-#pragma warning disable CS0618
-        [Fact]
-        public void MaxRequestBufferSizeIsMarkedObsolete()
-        {
-            Assert.NotNull(typeof(KestrelServerOptions)
-                .GetProperty(nameof(KestrelServerOptions.MaxRequestBufferSize))
-                .GetCustomAttributes(false)
-                .OfType<ObsoleteAttribute>()
-                .SingleOrDefault());
-        }
-
-        [Fact]
-        public void MaxRequestBufferSizeGetsLimitsProperty()
-        {
-            var o = new KestrelServerOptions();
-            o.Limits.MaxRequestBufferSize = 42;
-            Assert.Equal(42, o.MaxRequestBufferSize);
-        }
-
-        [Fact]
-        public void MaxRequestBufferSizeSetsLimitsProperty()
-        {
-            var o = new KestrelServerOptions();
-            o.MaxRequestBufferSize = 42;
-            Assert.Equal(42, o.Limits.MaxRequestBufferSize);
-        }
-#pragma warning restore CS0612
-
         [Fact]
         public void NoDelayDefaultsToTrue()
         {
